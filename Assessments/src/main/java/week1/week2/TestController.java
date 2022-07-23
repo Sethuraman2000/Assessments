659829955e8b088a7f40d40a62f7b0cbfa15c348
Sethuraman2000 <108717950+Sethuraman2000@users.noreply.github.com>
2022-07-23 17:45:48 +0530

    Update TestController.java

--- Assessments/src/main/java/week1/week2/TestController.java
+++ Assessments/src/main/java/week1/week2/TestController.java
@@ -5,7 +5,7 @@ public class TestController {
 	{
 	Employee e1=new Employee("Sethuraman M",21,"Associate Software Engineer");
 	Employee e2=new Employee("Yuvaraj G",21,"Associate Software Engineer");
-	Employee e3=new Employee("Sachin G",21,"Associate Software Engineer");
+	Employee e3=new Employee("Sachin D",21,"Associate Software Engineer");
 	
 	HR_Manager hr= new HR_Manager();
 	hr.process_Records(e1.name,e1.age,e1.designation);
